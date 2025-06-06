3453e2f397bb11955dcd84c53c54d5928e60c625
Karthik Boodidha <boodidhakarthik96@gmail.com>
2025-06-06 23:28:16 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,7 @@ from add_update_ui import add_update_tab
 from analytics_by_category_ui import  analytics_by_category_tab
 from analytics_by_month_ui import analytics_by_month_tab
 
-API_URL ="http://localhost:8000"
+API_URL ="http://localhost:8502"
 st.title("Expense Tracking System")
 tab1, tab2, tab3=st.tabs(["Add/Update","Analytics by Category","Analytics by Month"])
 with tab1:
